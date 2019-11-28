774e7252cf348dd2e85a773c51ad448e0de1b9e4
Ramazan <nakhalov.1996@mail.ru>
2019-11-28 23:33:48 +0300

    first commit

---
+++ function.js
@@ -0,0 +1,22 @@
+
+
+a = 5;
+console.log(a);
+
+let arr = ["dad.png","orange.jpg","applee.bmp"];
+
+//alert("Hello world");
+//let kod = alert();
+
+//console.log(typeof(arr[1]));
+let incr = 10,
+    decr = 10;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    let isCred = false,
+        isClose = false;
+
+console.log(isCred || !isCred);
+
