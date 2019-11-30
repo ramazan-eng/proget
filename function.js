20aab33c04f4e085c4fdea963e8a289ae246d4d3
Ramazan <nakhalov.1996@mail.ru>
2019-12-01 02:37:45 +0300

    while_for

--- function.js
+++ function.js
@@ -1,23 +1,38 @@
 
 
-let money = prompt("Ваш бюджет на месяц?", ''),
-time = prompt('Введите дату в формате YYYY-MM-DD', '');
+/*switch(num){
 
-let appData = {
-budget: money,
-expenses: {},
-optionalExpenses: {},
-income: [],
-timeData: time,
-savings: false
-};
+    case num > 49:
+        console.log("ДА 1 ");
+        break;
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-a2 = prompt("Во сколько обойдется?", ''),
-a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-a4 = prompt("Во сколько обойдется?", '');
+    case num < 30:
+        console.log("yes 2 ");
+        break;
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    case 50:
+        console.log("yes 3");
+        break;
 
-alert(appData.budget / 30); 
+        default:
+            console.log("Ошибка");
+            break;
+}
+*/
+
+//let num = 50; 
+//while (num < 100) {
+    //console.log(num);
+    //num++;
+//}
+
+//for (i = 1; i < 11; i++){
+//console.log(i);
+//}
+
+for (i = 1; i < 11; i++){
+    if (i == 6){
+        continue;
+    }
+    console.log(i);
+}
